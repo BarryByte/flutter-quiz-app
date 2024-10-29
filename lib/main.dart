c074e7784dfd477587364c61f8632b9e2a186b20
BarryByte <abhayraj.12667@gmail.com>
2024-10-30 00:05:30 +0530

    first page completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/pages/landingPage.dart';
 
-
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration:  const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end:Alignment.bottomRight,
+              colors: [Color.fromARGB(137, 165, 82, 82), Colors.purple],
+              // colors : [Colors.blue, Colors.yellow, Colors.pink, Colors.green],
+            ),
+          ),
+          child: FirstPage(),
+        ),
+      ),
+    ),
+  );
+}
---
+++ lib/pages/landingPage.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class FirstPage extends StatelessWidget {
+  const FirstPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        child: Column(
+          // crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          // mainAxisSize: MainAxisSize.min,
+          children: [
+            Opacity(
+              opacity: 0.7,
+              child: Image.asset(
+                'assets/images/quiz-logo.png',
+                width: 200,
+              ),
+            ),
+            const SizedBox(
+              height: 30.0,
+            ),
+            const Text(
+              "Learn Flutter the fun way",
+              style: TextStyle(color: Colors.white, fontSize: 18),
+            ),
+            const SizedBox(
+              height: 30.0,
+            ),
+            OutlinedButton.icon(
+              onPressed: () {},
+              style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
+              icon: Icon(Icons.arrow_forward_ios_rounded),
+              label: const Text('Start Quiz'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
